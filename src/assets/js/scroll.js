b7192d1dbb10770116dfb20a71748646d2a18139
David Deeley <dadeeley67@proton.me>
2023-12-20 16:07:38 -0500

    Finished dark mode changes, removed all the blog related stuff, formatted a lot of files

--- src/assets/js/scroll.js
+++ src/assets/js/scroll.js
@@ -1,19 +1,19 @@
 function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
-  
-    for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 150;
-  
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active");
-      } else {
-        continue;
-      }
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      continue;
     }
   }
-  
-  window.addEventListener("scroll", reveal);
+}
 
-  reveal();
+window.addEventListener("scroll", reveal);
+
+reveal();
